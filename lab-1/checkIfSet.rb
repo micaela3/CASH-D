a6338a6685c61f8f82f0ba12f11ef3e6f512883f
Catherine Quamme <quamme.3@osu.edu>
2021-09-19 10:36:04 -0400

    Added method that checks if 3 selected cards are a set or not.

---
+++ lab-1/checkIfSet.rb
@@ -0,0 +1,16 @@
+# Method that checks if 3 selected cards are a set or not.
+def checkIfSet(c1,c2,c3)
+
+    countAllSame = (c1.count == c2.count) && (c2.count == c3.count)
+    countAllDiff = (c1.count != c2.count) && (c2.count != c3.count) && (c1.count != c3.count)
+
+    shapeAllSame = (c1.shape == c2.shape && (c2.shape == c3.shape)
+    shapeAllDiff = (c1.shape != c2.shape) && (c2.shape != c3.shape) && (c1.shape != c3.shape)
+
+    shadingAllSame = (c1.shading == c2.shading) && (c2.shading == c3.shading)
+    shadingAllDiff = (c1.shading != c2.shading) && (c2.shading != c3.shading) && (c1.shading != c3.shading)
+
+    colorAllSame = (c1.color == c2.color) && (c2.color == c3.color)
+    colorAllDif = (c1.color != c2.color) && (c2.color != c3.color) && (c1.color != c3.color)
+
+    isSet = (countAllSame || countAllDiff) && (shapeAllSame || shapeAllDiff) && (shadingAllSame || shadingAllDiff) && (colorAllSame || colorAllDif)
