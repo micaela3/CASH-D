27c5b9ed88f3751c47591bde7f3797dfa887ea34
Darshit Vachhani <vachhani.8@osu.edu>
2021-09-19 11:04:04 -0400

    Added revisions to the main with methods implemented so far. Still working on implementing setPresent

--- lab-1/main.rb
+++ lab-1/main.rb
@@ -5,68 +5,120 @@ def main
     # welcome message for the user
     puts "Welcome to the game of set"
 
+
     # maybe display rules here
 
     # ask user to start when ready by inputting Y or N
-    puts "Are you ready to begin, Y or N?"
-    ready = gets.chomp
-        if ready != "Y"
+
+    puts "Are you ready to begin, y or n?"
+    ready = gets.chomp!
+
+        if ready != "y"
+
                 puts "Incorrect response, ending the game"
+
                 exit
+
         end
-    
+
     puts "Player 1: Enter your name"
-    player1 = gets.chomp
+    player1 = gets.chomp!
     puts "Player 2: Enter your name"
-    player2 = gets.chomp
+    player2 = gets.chomp!
 
     # Assign each player an alphabet to determine who chooses a set first
-    puts "#{player1} you will press 'A' to make a selection"
-    puts "#{player2} you will press 'O' to make a selection"
+
+    puts "#{player1} you will press 'a' to make a selection"
+    puts "#{player2} you will press 'o' to make a selection"
     puts "First player who sees a set will select their alphabet to make a selection"
 
-    # Array to hold the cards that will be displayed
-    chosenCards = []
+    # initial deck to contain the 81 cards
 
+    deck = []
 
     # filldeck method to fill the deck with 81 cards
+    fillDeck(deck)
 
     # shuffle method to shuffle all the cards
-
-    # pick first 12 cards from the array and call setpresent method
-    # to check if a set exists. If it doesn't add 3 cards and check again
-
-    # display the cards
-    dealCards(chosenCards)
+    shuffleDeck(deck)
 
     
-    # check which player goes first 
-    puts "Enter your alphabet:"
-    alphabet = get.chomp
 
-    while (alphabet != "A" || alphabet != "O")
+    # pick first 12 cards from the shuffled deck of cards and check if 
+    # a set is present. If a set is not present, add 3 more cards and check
+    # again. Continue adding a card until a set is present is in the deck.
+    # A set is guranteed to be present when there are 21 cards in the deck.
+    # Once a set is determined to be present, display the cards 
 
-        puts "Invalid input!, Please enter a valid input to continue:"
-        alphabet = get.chomp
-    
+    displayCards = chooseCards(deck)  
+
+    # continue the game until no cards are left
+    while (displayCards.length > 0) do
+
+        # display the cards
+        dealCards(displayCards)
+
+        # check which player goes first 
+        puts "Enter your alphabet:"
+        alphabet = get.chomp!
+
+        # check for valid input of selection alphabet
+        while alphabet != "a" || alphabet != "o" do
+
+            puts "Invalid input!, Please enter a valid input to continue:"
+            alphabet = get.chomp!
+
+        end
+
+        # ask for user input
+        puts "Input your response by inputting 1,2,3... seprated by a new line"
+
+        # store user input in an array (possibly use integers as identifiers)
+        input = []
+        input[0] = gets.chomp!
+        input[1] = gets.chomp!
+        input[2] = gets.chomp!
+
+        # checkifset method checks if the user selected cards make a set
+        isSet = checkifSet(input)
+
+        # check if the selection is a set 
+        while !isSet do
+
+          puts "The #{input[0]}, #{input[1]}, #{input[2]} is not a set. Any player can select their alphabet and make a selection"
+          puts "Enter your alphabet:"
+          alphabet = get.chomp!
+
+
+          while alphabet != "a" || alphabet != "o" do
+
+              puts "Invalid input!, Please enter a valid input to continue:"
+              alphabet = get.chomp!
+
+          end
+          puts "Input your response by inputting 1,2,3... seprated by a new line"
+          input[0] = gets.chomp!
+          input[1] = gets.chomp!
+          input[2] = gets.chomp!
+          isSet = checkifSet(input)
+
+        end
+
+        # remove the cards that a user selects that form a set
+        removeCards (displayCards, input[0], input[1], input[2])
+
+        # replace the cards that were taken out as a set
+        refreshCards(displayCards, deck)
+
     end
 
-    # ask for user input
-    puts "Input your response by inputting 1,2,3... seprated by a new line"
-
-    # store user input in an array (possibly use integers as identifiers)
-    input = []
-    input[0] = gets.chomp
-    input[1] = gets.chomp
-    input[2] = gets.chomp
-
-    # checkifset method checks if the user selected cards make a set
-
-    checkifSet(input)
 
 end
+
+
 
 # call main method
 
 main
+
                 
