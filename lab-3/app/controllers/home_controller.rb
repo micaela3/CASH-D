d7d3570a421a0ca06bf7600edff4b743d1558c79
Darshit Vachhani <vachhani.8@osu.edu>
2021-10-30 00:33:04 -0400

    Added the ascending order sorting based on the course number. Did some cleanup and removed some information not needed for TA view. Added the filter so that TA can select to only view 1xxx courses. Will add the other filters and comments soon. Updated the courses model for sorting and filtering. Will add the link as a comment to show where the piece of obtained from for filtering.

--- lab-3/app/controllers/home_controller.rb
+++ lab-3/app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @courses = Course.where(nil);
+    @courses = @courses.filter_by_starts_with(params[:starts_with]) if params[:starts_with].present?
   end
 end
--- lab-3/app/models/course.rb
+++ lab-3/app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
   has_many :sections
+  default_scope { order(course_number: :asc) }
+  scope :filter_by_starts_with, -> (course_number) { where("course_number like ?", "#{course_number}%")}
 end
