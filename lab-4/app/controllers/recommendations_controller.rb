fe6eb7754448fae791c1b5a83d6f212105bcdc12
Darshit Vachhani <vachhani.8@osu.edu>
2021-11-27 17:38:48 -0500

    Made some changes to view to give more specific instructions for input.

--- lab-4/app/controllers/recommendations_controller.rb
+++ lab-4/app/controllers/recommendations_controller.rb
@@ -7,17 +7,19 @@ class RecommendationsController < ApplicationController
  end 
   def new
     @recommendation = Recommendation.new 
-    @recommendation.grader_id = params[:recommendation][:grader_id]
+    @recommendation.instructor_name = params[:recommendation][:instructor_name]
+    @recommendation.grader_name = params[:recommendation][:grader_name]
     @recommendation.instructor_id = params[:recommendation][:instructor_id]
+    @recommendation.grader_id = params[:recommendation][:grader_id] 
     @recommendation.course = params[:recommendation][:course]
     @recommendation.section = params[:recommendation][:section]
-    @recommendation.comments = params[:recommendation][:comments] 
-    @recommendation.grader_name = params[:recommendation][:grader_name]
-    @recommendation.instructor_name = params[:recommendation][:instructor_name]
     @recommendation.comments = params[:recommendation][:comments]
     
-    @recommendation.save
+  if @recommendation.save
+    redirect_to 'home_index_path'
   end
+  end
+
 end
 
 
