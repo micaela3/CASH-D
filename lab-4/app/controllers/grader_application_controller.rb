5568cf2742b37ee72b87b9d318170e0b3a5b9a66
Catherine Quamme <catherinequamme@hotmail.com>
2021-11-28 13:56:27 -0500

    Grader form is fully editable and working. Only thing currently missing from form is way to delete available time slots and course interest slots.

--- lab-4/app/controllers/grader_application_controller.rb
+++ lab-4/app/controllers/grader_application_controller.rb
@@ -5,8 +5,7 @@ class GraderApplicationController < ApplicationController
       @currAvails = Availability.where(grader:@grader)
       @courseInts = CoursesGrader.where(grader:@grader)
     else 
-
-
+      @grader = Grader.new
     end
 
     @courses = Course.all
@@ -18,30 +17,68 @@ class GraderApplicationController < ApplicationController
 
   def post
 
+    @grader = Grader.new
+    @grader.name = params[:grader][:name]
+    @grader.email = params[:grader][:email]
+    @grader.major = params[:grader][:major]
+    @grader.user = current_user
+
+    @availabilities = params[:availability]
+
+    @course_interests = params[:courses]
+
     if current_user.has_grader
-      
-    else 
-      @grader = Grader.new
-      @grader.name = params[:name]
-      @grader.email = params[:email]
-      @grader.major = params[:major]
-      @grader.user = current_user
-      @grader.save
-      
-      @availabilities = params[:availability]
+
+      @currGrader = Grader.find_by user:current_user
+      Availability.where(grader:@currGrader).delete_all
+
+      CoursesGrader.where(grader:@currGrader).delete_all   
+
+
+      @currGrader.name = @grader.name
+      @currGrader.email = @grader.email
+      @currGrader.major = @grader.major
+
+      @currGrader.save 
 
       @availabilities.each do |index, value|
         @availability = Availability.new
+  
+        @availability.weekday = @availabilities[index][:weekday]
+        @availability.start_time = @availabilities[index][:start_time]
+        @availability.end_time = @availabilities[index][:end_time]
+        @availability.grader = @currGrader
+  
+        @availability.save
+      end
+      
+     
+      @course_interests.each do |index, value|
+          @courses_graders = CoursesGrader.new
+          
+          @courses_graders.course_id = @course_interests[index][:course_id]
+          @courses_graders.grader = @currGrader
+          
+          @courses_graders.save
+      end
 
+      
+    else 
+
+      @grader.save     
+
+      @availabilities.each do |index, value|
+        @availability = Availability.new
+  
         @availability.weekday = @availabilities[index][:weekday]
         @availability.start_time = @availabilities[index][:start_time]
         @availability.end_time = @availabilities[index][:end_time]
         @availability.grader = @grader
-
+  
         @availability.save
       end
       
-      @course_interests = params[:courses]
+     
       @course_interests.each do |index, value|
           @courses_graders = CoursesGrader.new
           
@@ -53,7 +90,8 @@ class GraderApplicationController < ApplicationController
       
       current_user.has_grader = true
       current_user.save
-      end    
+    end    
+
   end
 
 end
--- lab-4/app/javascript/packs/graderapp.js
+++ lab-4/app/javascript/packs/graderapp.js
@@ -1,31 +1,21 @@
 var index = 0;
-window.addNewTimeSlot = function(weekday=0, startTime=0, endTime=0){
-
-    startTime = new Date(Date.parse(startTime));
-    endTime = new Date(Date.parse(endTime));
+window.addNewTimeSlot = function(weekday=0, startTime=0, endTime=0){ 
     
-
-
     var form = document.getElementById('availability_template').cloneNode(true);
     form.id = "";
     form.style.display = 'flex';
     form.querySelector('#availability_weekday').setAttribute('name','availability['+(index)+'][weekday]');
-    
-    form.querySelector('#availability_weekday').selectedIndex=weekday; 
-   
-
     form.querySelector('#availability_start_time').setAttribute('name','availability['+(index)+'][start_time]');
-    form.querySelector('#availability_start_time').value=startTime.toISOString().substring(11,16); 
+    form.querySelector('#availability_end_time').setAttribute('name','availability['+(index)+'][end_time]');   
+
+    if (startTime!=0 && endTime !=0) {
+        startTime = new Date(Date.parse(startTime));
+        endTime = new Date(Date.parse(endTime));
+        form.querySelector('#availability_weekday').selectedIndex=weekday;
+        form.querySelector('#availability_start_time').value=startTime.toISOString().substring(11,16); 
+        form.querySelector('#availability_end_time').value=endTime.toISOString().substring(11,16); 
+    }
   
-
-
-    form.querySelector('#availability_end_time').setAttribute('name','availability['+(index)+'][end_time]');
-    form.querySelector('#availability_end_time').value=endTime.toISOString().substring(11,16); 
-  
-
-
-
-
     var list = document.getElementById('availabilities');
     index++;
     list.append(form);
@@ -38,9 +28,7 @@ window.addNewCourse = function(courseID=undefined) {
     form.id = "";
     form.style.display = 'flex';
     form.querySelector('#course_id').setAttribute('name','courses['+(courseIndex)+'][course_id]');
-    form.querySelector('#course_id').selectedIndex=courseID;
-
-
+    form.querySelector('#course_id').value=courseID;
 
     courseIndex++;
     var list = document.getElementById('courses');
