8511d1cf0487b7f9184e3de0c540c7941a1fb3b8
Catherine Quamme <catherinequamme@hotmail.com>
2021-12-03 11:11:49 -0500

    Grader form redirects to home page on submission

--- lab-4/app/controllers/grader_application_controller.rb
+++ lab-4/app/controllers/grader_application_controller.rb
@@ -25,74 +25,63 @@ class GraderApplicationController < ApplicationController
     @grader.user = current_user
 
     @availabilities = params[:availability]
-
     @course_interests = params[:courses]
 
     if current_user.has_grader
+        @currGrader = Grader.find_by user:current_user
+        Availability.where(grader:@currGrader).delete_all
+        CoursesGrader.where(grader:@currGrader).delete_all   
 
-      @currGrader = Grader.find_by user:current_user
-      Availability.where(grader:@currGrader).delete_all
+        @currGrader.name = @grader.name
+        @currGrader.email = @grader.email
+        @currGrader.major = @grader.major
+        @currGrader.name_dot_number = @grader.name_dot_number
 
-      CoursesGrader.where(grader:@currGrader).delete_all   
+        @currGrader.save 
 
-
-      @currGrader.name = @grader.name
-      @currGrader.email = @grader.email
-      @currGrader.major = @grader.major
-      @currGrader.name_dot_number = @grader.name_dot_number
-
-      @currGrader.save 
-
-      @availabilities.each do |index, value|
-        @availability = Availability.new
-  
-        @availability.weekday = @availabilities[index][:weekday]
-        @availability.start_time = @availabilities[index][:start_time]
-        @availability.end_time = @availabilities[index][:end_time]
-        @availability.grader = @currGrader
-  
-        @availability.save
-      end
-      
+        @availabilities.each do |index, value|
+          @availability = Availability.new  
+          @availability.weekday = @availabilities[index][:weekday]
+          @availability.start_time = @availabilities[index][:start_time]
+          @availability.end_time = @availabilities[index][:end_time]
+          @availability.grader = @currGrader  
+          @availability.save
+      end      
      
       @course_interests.each do |index, value|
-          @courses_graders = CoursesGrader.new
-          
+          @courses_graders = CoursesGrader.new          
           @courses_graders.course_id = @course_interests[index][:course_id]
-          @courses_graders.grader = @currGrader
-          
+          @courses_graders.grader = @currGrader          
           @courses_graders.save
       end
-
       
     else 
+        @grader.save     
 
-      @grader.save     
+        @availabilities.each do |index, value|
+          @availability = Availability.new  
+          @availability.weekday = @availabilities[index][:weekday]
+          @availability.start_time = @availabilities[index][:start_time]
+          @availability.end_time = @availabilities[index][:end_time]
+          @availability.grader = @grader  
+          @availability.save
+        end     
+      
+        @course_interests.each do |index, value|
+            @courses_graders = CoursesGrader.new          
+            @courses_graders.course_id = @course_interests[index][:course_id]
+            @courses_graders.grader = @grader          
+            @courses_graders.save
+        end
+        
+        current_user.has_grader = true
 
-      @availabilities.each do |index, value|
-        @availability = Availability.new
-  
-        @availability.weekday = @availabilities[index][:weekday]
-        @availability.start_time = @availabilities[index][:start_time]
-        @availability.end_time = @availabilities[index][:end_time]
-        @availability.grader = @grader
-  
-        @availability.save
-      end
-      
-     
-      @course_interests.each do |index, value|
-          @courses_graders = CoursesGrader.new
-          
-          @courses_graders.course_id = @course_interests[index][:course_id]
-          @courses_graders.grader = @grader
-          
-          @courses_graders.save
-      end
-      
-      current_user.has_grader = true
-      current_user.save
-    end    
+        current_user.save
+        
+        
+    
+    end 
+    redirect_to root_path, notice: "Form submission successful!"   
 
   end
 
